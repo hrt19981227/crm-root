d0c32745be64ffc9afb217f8fcb173b8ecc5f40a
taoge <haorongtao1998@163.com>
2021-01-06 12:03:27 +0800

    解决跨域问题以及端口变更9000

---
+++ src/main/java/com/qy25/sm/config/MyFilter.java
@@ -0,0 +1,16 @@
+package com.qy25.sm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MyFilter implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(false)
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
